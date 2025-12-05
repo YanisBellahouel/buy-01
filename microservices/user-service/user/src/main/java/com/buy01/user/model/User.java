0f4077ec8cbeebcbc7652368e897109eb59c6592
ybellaho <yanis.bellahouel76@gmail.com>
2025-12-05 09:38:05 +0100

    user services models

---
+++ microservices/user-service/user/src/main/java/com/buy01/user/model/User.java
@@ -0,0 +1,35 @@
+package com.buy01.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String password;
+
+    private Role role;
+
+    private String avatar;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
