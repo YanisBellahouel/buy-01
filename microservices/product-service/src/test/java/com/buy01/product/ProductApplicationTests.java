24d5bd39233279ef03456ff439f4d17f3099c2f3
ybellaho <yanis.bellahouel76@gmail.com>
2026-02-20 09:14:48 +0100

    test email

--- microservices/product-service/src/test/java/com/buy01/product/ProductApplicationTests.java
+++ microservices/product-service/src/test/java/com/buy01/product/ProductApplicationTests.java
@@ -3,13 +3,13 @@ package com.buy01.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ProductApplicationTests {
 
     @Test
     void contextLoads() {
-        assertTrue(false, "Intentional failure for CI audit");
+        // assertTrue(false, "Intentional failure for CI audit");
     }
 }
